1b216e00712cf810ab76ca9d8411841a51331cd9
Yauheni Khotsin <yauheni_khotsin@epam.com>
2020-01-05 16:43:11 +0300

    Add parsing logic unit tests.

--- LocationsParser.Services/LocationStringParserService.cs
+++ LocationsParser.Services/LocationStringParserService.cs
@@ -8,19 +8,39 @@ namespace LocationsParser.Services
 {
     public static class LocationStringParserService
     {
+        private const string _cityPrefix = "Rooms ";
+        private const string _officeNamePrefix = " ";
+        private const char _separator = ',';
+        
         public static string GetCityName(LocationDTO locationDTO)
         {
-            return locationDTO.Name.Split(',')[0].Substring("Rooms ".Length);
+            var cityStringWithPrefix = locationDTO.Name.Split(_separator)[0];
+
+            if (cityStringWithPrefix.Length < _cityPrefix.Length ||
+                cityStringWithPrefix.Substring(0, _cityPrefix.Length) != _cityPrefix)
+            {
+                throw new ArgumentException($"Impossible to get city name from \"{locationDTO.Name}\"");
+            }
+
+            return cityStringWithPrefix.Substring(_cityPrefix.Length);
         }
 
         public static string GetOfficeName(LocationDTO locationDTO)
         {
-            var parts = locationDTO.Name.Split(',');
+            var parts = locationDTO.Name.Split(_separator);
 
             if (parts.Length < 2)
                 return null;
 
-            return parts[1].Substring(" ".Length);
+            var officeNameStringWithPrefix = parts[1];
+
+            if (officeNameStringWithPrefix.Length < _officeNamePrefix.Length ||
+                officeNameStringWithPrefix.Substring(0, _officeNamePrefix.Length) != _officeNamePrefix)
+            {
+                throw new ArgumentException($"Impossible to get office name from \"{locationDTO.Name}\"");
+            }
+
+            return officeNameStringWithPrefix.Substring(_officeNamePrefix.Length);
         }
     }
 }
