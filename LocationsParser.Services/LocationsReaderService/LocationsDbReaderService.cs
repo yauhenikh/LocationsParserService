9898d2e57d1b357406ea70ae620654d83e8f3700
Yauheni Khotsin <yauheni_khotsin@epam.com>
2020-01-05 17:16:16 +0300

    Merge add-reader-service. Add LocationsDbReaderService class.

---
+++ LocationsParser.Services/LocationsReaderService/LocationsDbReaderService.cs
@@ -0,0 +1,40 @@
+﻿using LocationsParser.DataAccess.Data;
+using LocationsParser.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocationsParser.Services
+{
+    public class LocationsDbReaderService : ILocationsReaderService
+    {
+        private readonly AppDbContext _dbContext;
+
+        public LocationsDbReaderService(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<Location> GetAllLocations()
+        {
+            return _dbContext.Locations.ToList();
+        }
+
+        public Location GetLocationByUniqueId(string uniqueId)
+        {
+            return _dbContext.Locations.Where(l => l.UniqueIdentifier == uniqueId).SingleOrDefault();
+        }
+
+        public Location GetLocationByEmail(string email)
+        {
+            return _dbContext.Locations.Where(l => l.Email == email).FirstOrDefault();
+        }
+
+        public List<Location> GetLocationsByCity(string city)
+        {
+            return _dbContext.Locations.Where(l => l.City.Name == city).ToList();
+        }
+    }
+}
