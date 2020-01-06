b40aa7a95474b0889217b3514ea2333b241db82e
Yauheni Khotsin <yauheni_khotsin@epam.com>
2020-01-06 14:19:56 +0300

    Add LocationsParser.ConsoleCLI project to read data from database.

---
+++ LocationsParser.ConsoleCLI/Commands/CommandLineApplicationWithDI.cs
@@ -0,0 +1,34 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocationsParser.ConsoleCLI.Commands
+{
+    public class CommandLineApplicationWithDI : CommandLineApplication
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public CommandLineApplicationWithDI(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+            RegisterCommands();
+        }
+
+        private void RegisterCommands()
+        {
+            foreach (var command in _serviceProvider.GetServices<ICommand>())
+            {
+                var commandLineApp = command as CommandLineApplication;
+
+                if (commandLineApp == null)
+                {
+                    throw new InvalidCastException("Commands must inherit from ICommand and CommandLineApplication");
+                }
+
+                Commands.Add(commandLineApp);
+            }
+        }
+    }
+}
---
+++ LocationsParser.ConsoleCLI/Commands/GetByCityCommand.cs
@@ -0,0 +1,31 @@
+﻿using LocationsParser.Services;
+using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocationsParser.ConsoleCLI.Commands
+{
+    internal class GetByCityCommand : CommandLineApplication, ICommand
+    {
+        private readonly ILocationsReaderService _locationsReaderService;
+        private readonly CommandArgument _argument;
+
+        public GetByCityCommand(ILocationsReaderService locationsReaderService)
+        {
+            _locationsReaderService = locationsReaderService;
+            Name = "getbycity";
+            _argument = Argument("city", "city of location").IsRequired();
+            Description = "gets locations by city";
+            HelpOption("-? | -h | --help");
+            OnExecute((Func<int>)Execute);
+        }
+
+        public int Execute()
+        {
+            var locations = _locationsReaderService.GetLocationsByCity(_argument.Value);
+            Helpers.PrintLocations(locations);
+            return 0;
+        }
+    }
+}
